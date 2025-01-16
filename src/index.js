386760a03349922e43f0b57c2ba372e4aed5aa4d
sinkong2 <dhroqneo@naver.com>
2025-01-16 17:27:03 +0900

    2025-01-16일 학습내용

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import ReactDOM from 'react-dom/client';
+// import App from './study/App';
+// import App2 from './study/App2';
+// import App3 from './study/App3';
+// import App4 from './study/App4';
+// import App5 from './study/App5';
+// import App6 from './study/App6';
+// import App7 from './study/App7';
+// import App8 from './study/App8'
+// import App9 from './study/App9'
+import App10 from './study/App10';
+// import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// root.render(<App />);
+// root.render(<App2 />);
+// root.render(<App3 />);
+root.render(< App10 />);
+
---
+++ src/study/App10.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react';
+
+
+function App10(props) {
+    const[ num1, setNum1 ] = useState(0);
+    const[ num2, setNum2 ] = useState(0);
+
+    const unmount = () => {
+        console.log("장착해제됨");
+    }
+
+    const mount = () => {
+        console.log("장착됨2");
+        return unmount;
+    }
+
+
+    useEffect(mount); // [] 의 역할 > useEffect의 의존성을 가지게 함
+
+    useEffect(() => {
+        console.log(num1);
+        setNum2(num1 + 100);
+    }, [num1]); // 함수첨부와 더불어 변수값을 지정가능([])
+
+    useEffect(() => {
+        console.log(num2);
+    }, [num2]);
+
+    useEffect(() => {
+        console.log(num1);
+        console.log(num2);
+    }, [num1,num2]);
+
+    useEffect(() => {
+        console.log("마운트!!");
+    },[]);
+
+    const hadleOnClick = () => {
+        setNum1(num1+10);
+    }
+
+    return (
+        <div>
+            <h1>Num1:{num1}</h1>
+            <h1>Num2:{num2}</h1>
+
+            <button onClick={hadleOnClick}>클릭</button>
+            
+        </div>
+    );
+}
+
+export default App10;
